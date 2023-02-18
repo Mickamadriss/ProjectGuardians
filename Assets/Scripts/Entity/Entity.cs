b86fce74bf739f53ff59a8347cfe3340667dfc79
IdrissB77 <59149839+IdrissB77@users.noreply.github.com>
2023-02-18 18:10:15 +0100

    entities prennent des dégâts. /!\ c'est à fixe plus tard

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -3,17 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Entity : MonoBehaviour
+public abstract class Entity : MonoBehaviour, IDamageable
 {
     [SerializeField] private int life;
-    
+
+    public void TakeDamage(int damage)
+    {
+        life -= damage;
+        if(life <= 0)
+        {
+            Destroy(this); // ATTENTION PAS DUTOUT OPTIMAL, déjà psk le player ne peut pas disparaître comme ça
+        }
+    }
+
     private void heal(int n)
     {
         life += n;
-    }
-
-    private void takeDamage(int n)
-    {
-        life -= n;
     }
 }
